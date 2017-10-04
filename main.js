92dacfd2cf9a45961fb7c6df2c17f8f6f79e7bf8
Karl Hepler <khepler@brandmovers.com>
2017-10-03 21:42:46 -0400

    text is displaying & color changes are in place

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 (function (d3, Math, undefined) {
 
     var data = [
-        { title: 'Foo' },
-        { title: 'Foo' },
-        { title: 'Foo' },
-        { title: 'Foo' },
-        { title: 'Foo' },
-        { title: 'Foo' },
-        { title: 'Foo' },
-        { title: 'Foo' },
+        { label: 'charity', color: 'rgb(62, 165, 218)' },
+        { label: 'income planning', color: 'rgb(137, 194, 82)' },
+        { label: 'asset management', color: 'rgb(68, 176, 84)' },
+        { label: 'tax diversification', color: 'rgb(34, 139, 75)' },
+        { label: 'risk management', color: 'rgb(24, 99, 59)' },
+        { label: 'credit management', color: 'rgb(55, 175, 115)' },
+        { label: 'legacy planning', color: 'rgb(41, 162, 150)' }
     ];
 
     var diameter = 500;
     var arrow = 0.1;
     var margin = 5;
     var radius = {
-        inner: diameter / 4 - margin/2,
+        inner: diameter / 4 - margin / 2,
         midway: diameter / 4 + diameter / 8,
         outer: diameter / 2 + margin/2,
     };
@@ -23,137 +22,82 @@
     // The SVG container
     var svg = d3.select('.chart')
         .append('svg')
-        .attr('width', diameter)
-        .attr('height', diameter)
-        .append('g')
-        .attr('transform', 'translate(' + (radius.outer-margin/2) + ',' + (radius.outer-margin/2) + ')')
-        .attr('fill', 'black')
-        .attr('stroke', 'white')
-        .attr('stroke-width', margin);
+            .attr('width', diameter)
+            .attr('height', diameter)
+            .append('g')
+                .attr('transform', 'translate(' + (radius.outer-margin/2) + ',' + (radius.outer-margin/2) + ')');
 
-    svg.selectAll('path')
+    // The sections of the doughnut
+    var sections = function (datum, index, paths) {
+        var angle = 2 * Math.PI / paths.length;
+        var nextIndex = index + 1;
+
+        // Calculate & save the centroid
+        datum.centroid = centroid(
+            angle*index, angle*nextIndex,
+            radius.inner, radius.outer 
+        );
+
+        var path = d3.path();
+
+        // Start
+        path.moveTo(
+            radius.outer * Math.cos(angle*index),
+            radius.outer * Math.sin(angle*index)
+        );
+
+        // Curve outer radius
+        path.arc(0, 0, radius.outer, angle*index, angle*nextIndex);
+
+        // Half arrow line to midway radius
+        path.lineTo(
+            radius.midway * Math.cos(angle*nextIndex+arrow),
+            radius.midway * Math.sin(angle*nextIndex+arrow)
+        );
+
+        // Curve inner radius
+        path.arc(0, 0, radius.inner, angle*nextIndex, angle*index, true);
+
+        // Half arrow line to outer radius
+        path.lineTo(
+            radius.midway * Math.cos(angle*index+arrow),
+            radius.midway * Math.sin(angle*index+arrow)
+        );
+
+        // Close the path
+        path.closePath();
+
+        return path;
+    }
+
+    var g = svg.selectAll('path')
         .data(data)
         .enter()
-            .append('path')
-            .attr('d', function (datum, index, paths) {
-                var angle = 2 * Math.PI / paths.length;
-                var nextIndex = index + 1;
+            .append('g');
 
-                var path = d3.path();
+    g.append('path')
+        .attr('fill', function (datum) { return datum.color; })
+        .attr('stroke', '#ffffff')
+        .attr('stroke-width', margin)
+        .attr('d', sections);
 
-                // Start
-                path.moveTo(
-                    radius.outer * Math.cos(angle*index),
-                    radius.outer * Math.sin(angle*index)
-                );
+    g.append('text')
+        .attr('fill', '#ffffff')
+        .attr('font-size', '10px')
+        .attr('font-family', 'sans-serif')
+        .attr('transform', function (datum) {
+            let x = datum.centroid[0];
+            let y = datum.centroid[1];
+            let mid = datum.label.length / 2;
+            datum.centroid[0] = x - mid*7;
+            return 'translate(' + datum.centroid + ')';
+        })
+        .text(function (datum) { return datum.label.toUpperCase(); })
 
-                // Curve outer radius
-                path.arc(0, 0, radius.outer, angle*index, angle*nextIndex);
-
-                // Half arrow line to midway radius
-                path.lineTo(
-                    radius.midway * Math.cos(angle*nextIndex+arrow),
-                    radius.midway * Math.sin(angle*nextIndex+arrow)
-                );
-
-                // Curve inner radius
-                path.arc(0, 0, radius.inner, angle*nextIndex, angle*index, true);
-
-                // Half arrow line to outer radius
-                path.lineTo(
-                    radius.midway * Math.cos(angle*index+arrow),
-                    radius.midway * Math.sin(angle*index+arrow)
-                );
-
-                // Close the path
-                path.closePath();
-
-                return path;
-            });
-
-    // The line SVG path we draw
-    // var lineGraph = svgContainer.append('path')
-        // .attr('d', lineFunction(6))
-        // .attr('stroke', 'blue')
-        // .attr('stroke-width', 2)
-        // .attr('fill', 'none');
-
-    // var data = [
-        // { fruit: 'Apple', count: 10 },
-        // { fruit: 'Strawberry', count: 20 },
-        // { fruit: 'Banana', count: 15 },
-        // { fruit: 'Blueberry', count: 20 },
-        // { fruit: 'Kiwi', count: 30 },
-        // { fruit: 'Plum', count: 5 },
-    // ];
-
-    // // Setup the margin & circle radius
-    // var padding = 10;
-    // var width = 500;
-    // var height = 500;
-    // var radius = width / 2;
-
-    // // Define svg
-    // var svg = d3.select('.chart')
-        // .append('svg')
-        // .attr('width', width)
-        // .attr('height', height)
-        // .append('g')
-        // .attr('transform', 'translate(' + width/2 + ',' + height/2 + ')');
-
-    // // var color = d3.scaleOrdinal()
-        // // .range(['#bbdefb', '#98caf9', '#64b5f6', '#42a5f5', '#2196f3', '#1e88e5', '#1976d2']);
-
-    // // Generate the arc
-    // var arc = d3.arc()
-        // .outerRadius(radius - padding)
-        // .innerRadius((radius - padding) - 50)
-        // .padAngle(0.025);
-
-    // // // Label arc
-    // // var labelArc = d3.arc()
-        // // .outerRadius(radius - padding - 100)
-        // // .innerRadius((radius - padding) - 200);
-
-    // // Generate the pie
-    // // var pie = d3.pie()
-        // // .sort(null)
-        // // .value(data.length);
-
-    // // Append g elements (arc)
-    // var g = svg.selectAll('.arc')
-        // .data(data))
-        // .enter()
-            // .append('g')
-            // .attr('class', 'arc');
-
-    // // Append the path of the arc
-    // g.append('path')
-        // .attr('d', arc)
-        // .style('fill', 'red');
-        // // .transition()
-        // // .ease(d3.easeLinear)
-        // // .duration(1000)
-        // // .attrTween('d', pieTween);
-
-    // var centroid = function (d) {
-        // return [(d.startAngle + d.endAngle) / 2, 0];
-    // };
-
-    // // Append the text (label)
-    // // g.append('text')
-        // // .attr('transform', function (d) { console.log(d, arc.centroid(d), centroid(d)); return 'translate(' + centroid(d) + ')'; })
-        // // .text('+');
-
-    // // g.append('text')
-        // // .attr('transform', function (d) { return 'translate(' + arc.centroid(d) + ')'; })
-        // // .text('+');
-
-    // // function pieTween(b) {
-        // // b.innerRadius = 0;
-        // // var i = d3.interpolate({ startAngle: 0, endAngle: 0 }, b);
-        // // return function (t) { return arc(i(t)); };
-    // // }
-
+    function centroid(startAngle, endAngle, innerRadius, outerRadius) {
+        var r = (innerRadius + outerRadius) / 2,
+            a = (startAngle + endAngle) / 2;
+        return [Math.cos(a) * r, Math.sin(a) * r];
+    }
 
 })(window.d3, window.Math);
