9c7a0bd52c353807db223a7941b9b95e2868a11b
Karl Hepler <khepler@brandmovers.com>
2017-10-08 18:26:35 -0400

    revamped

--- main.js
+++ main.js
@@ -1,86 +1,173 @@
-(function (d3, Math, location, undefined) {
+var Graph;
 
-    var data = [
-        { label: 'charity', color: 'rgb(62, 165, 218)', href: 'https://google.com' },
-        { label: 'income planning', color: 'rgb(137, 194, 82)', href: 'https://facebook.com' },
-        { label: 'asset management', color: 'rgb(68, 176, 84)', href: 'http://karlhepler.com' },
-        { label: 'tax diversification', color: 'rgb(34, 139, 75)', href: 'http://integrity.financial' },
-        { label: 'risk management', color: 'rgb(24, 99, 59)', href: 'https://apple.com' },
-        { label: 'credit management', color: 'rgb(55, 175, 115)', href: 'https://reddit.com' },
-        { label: 'legacy planning', color: 'rgb(41, 162, 150)', href: 'http://foodwishes.blogspot.com' }
-    ];
+(function (d3, sin, cos, TAU, SQRT2, undefined) {
+    'use strict';
 
-    //----- SETTINGS -----------------------------------// 
-    var diameter = 500;
-    var arrow = 0.2;
-    var margin = 30;
-    var radius = { outer: diameter / 2 - margin };
-    radius.inner = radius.outer / 3 + margin;
-    radius.midway = radius.outer / 2 + radius.inner / 2;
-    var fontSize = diameter * 0.02;
-    var padding = 3;
-    var padAngle = {
-        outer: padding / radius.outer,
-        midway: padding / radius.midway,
-        inner: padding / radius.inner,
-    };
-    var arrowAngle = 0.1;
+        //----- SETTINGS -----------------------------------// 
+    var DIAMETER = 500,
+        INNER_RATIO = 0.5,
+        GRAPH_PADDING = 30,
+        ARC_PADDING = 3,
+        ARROW_ANGLE = 0.1,
+        SECTION_LOAD_DELAY_MS = 100,
+        FONT_SIZE_RATIO = 0.02,
+        MAX_CHARS_PER_LINE = 10,
 
-    //----- CREATE THE SVG CONTAINER -------------------// 
-    var svg = d3.select('.chart')
-        .append('svg')
-            .attr('width', diameter)
-            .attr('height', diameter)
-            .append('g')
-                .attr('transform', 'translate(' + (radius.outer + margin) + ',' + (radius.outer + margin) + ')');
+        //----- CALCULATIONS -------------------------------// 
+        FONT_SIZE = DIAMETER * FONT_SIZE_RATIO,
+        OUTER_RADIUS = DIAMETER / 2 - GRAPH_PADDING,
+        OUTER_PAD_ANGLE = ARC_PADDING / OUTER_RADIUS,
+        INNER_RADIUS = OUTER_RADIUS - OUTER_RADIUS * INNER_RATIO,
+        INNER_PAD_ANGLE = ARC_PADDING / INNER_RADIUS,
+        MIDDLE_RADIUS = OUTER_RADIUS / 2 + INNER_RADIUS / 2,
+        MIDDLE_PAD_ANGLE = ARC_PADDING / MIDDLE_RADIUS;
 
-    //----- CREATE A G FOR EACH DATUM ------------------// 
-    var g = svg.selectAll('path')
-        .data(data)
-        .enter()
+    //----- GRAPH CONSTRUCTOR --------------------------// 
+    Graph = function Graph(DATA) {
+        var ARC_ANGLE = TAU / DATA.length;
+
+        /**
+         * USE D3.JS TO BUILD THE GRAPH
+         */
+
+        //----- CREATE SVG CONTAINER WITH INNER GROUP ------// 
+        var svg = d3.select('.chart')
+            .append('svg')
+                .attr('width', DIAMETER)
+                .attr('height', DIAMETER)
+                .append('g')
+                    .attr('transform', getChartTranslation);
+
+        //----- CREATE A GROUP FOR EACH DATUM --------------// 
+        var dataGroup = svg.selectAll('path')
+            .data(DATA).enter()
             .append('g')
                 .attr('class', 'section hidden')
-                .on('mouseenter', function (datum) {
-                    this.classList.remove('bounceIn');
-                    this.classList.remove('pulse');
-                    setTimeout(function () { this.classList.add('pulse'); }.bind(this))
-                    setTimeout(function () { this.classList.remove('pulse'); }.bind(this), 1000);
-                })
-                .on('click', function (datum) {
-                    this.classList.remove('jello');
-                    setTimeout(function () { this.classList.add('jello'); }.bind(this));
-                    setTimeout(function () { this.classList.remove('jello'); }.bind(this), 1000);
-                    console.log(datum);
-                });
+                .on('mouseenter', onSectionMouseEnter)
+                .on('click', onSectionClick);
 
-    //----- CREATE THE PATH & THROW IN THE SECTIONS ----// 
-    var path = g.append('path')
-        .attr('fill', function (datum) { return datum.color; })
-        .attr('d', section);
+        //----- LOADING ANIMATION FOR EACH SECTION ---------// 
+        dataGroup.transition()
+            .delay(getSectionLoadDelay)
+            .attr('class', 'section animated bounceIn');
 
-   //----- DO SOME ANIMATIONS -------------------------// 
-    g.transition()
-        .delay(function (datum, index) { return index * 100; })
-        .attr('class', 'section animated bounceIn');
+        //----- CREATE THE PATH & INSERT THE SECTIONS ------// 
+        dataGroup.append('path')
+            .attr('fill', getSectionFillColor)
+        	.attr('d', getSectionPath);
 
-    //----- CREATE TEXT AROUND CENTROID ----------------// 
-    var text = g.append('text')
-        .attr('font-size', function () { return fontSize; })
-        .attr('transform', function (datum) {
+        //----- CREATE TEXT AROUND CENTROID ----------------// 
+        var text = dataGroup.append('text')
+            .attr('font-size', FONT_SIZE)
+            .attr('transform', getTextTranslation);
+
+        //----- WORD WRAPPING ------------------------------// 
+        text.selectAll('tspan')
+            .data(getTextLines).enter()
+            .append('tspan')
+                .attr('x', 0)
+                .attr('y', getAdjustedTextTop)
+                .text(getLine);
+
+        /**
+         * HELPER FUNCTIONS USED TO CREATE THE GRAPH
+         */
+
+        //----- CALCULATE THE CHART'S TRANSLATION ----------// 
+        function getChartTranslation() {
+            var translation = [
+                OUTER_RADIUS + GRAPH_PADDING,
+                OUTER_RADIUS + GRAPH_PADDING,
+            ];
+            return 'translate(' + translation + ')';
+        }
+
+        //----- ON SECTION MOUSE ENTER ---------------------// 
+        function onSectionMouseEnter() {
+            this.classList.remove('bounceIn');
+            this.classList.remove('pulse');
+            setTimeout(function () { this.classList.add('pulse'); }.bind(this))
+            setTimeout(function () { this.classList.remove('pulse'); }.bind(this), 1000);
+        }
+
+        //----- ON SECTION CLICK  --------------------------// 
+        function onSectionClick(datum) {
+            this.classList.remove('jello');
+            setTimeout(function () { this.classList.add('jello'); }.bind(this));
+            setTimeout(function () { this.classList.remove('jello'); }.bind(this), 1000);
+            console.log(datum);
+        }
+
+        //----- GET SECTION LOAD DELAY ---------------------// 
+        function getSectionLoadDelay(datum, index) {
+            return index * SECTION_LOAD_DELAY_MS;
+        }
+
+        //----- GET SECTION FILL COLOR ---------------------// 
+        function getSectionFillColor(datum) {
+            return datum.color;
+        }
+
+        //----- GET THE SECTION ----------------------------// 
+        function getSectionPath(datum, index) {
+            var angle = ARC_ANGLE * index,
+                delta = ARC_ANGLE * (index + 1);
+
+            // Calculate the text centroid
+            var centroidAngle = ARROW_ANGLE + (angle + delta) / 2;
+            datum.centroid = [
+                MIDDLE_RADIUS * cos(centroidAngle),
+                MIDDLE_RADIUS * sin(centroidAngle),
+            ];
+
+            // Create the new path
+            var path = d3.path();
+
+            // Start
+            path.moveTo(
+                OUTER_RADIUS * cos(angle + OUTER_PAD_ANGLE),
+                OUTER_RADIUS * sin(angle + OUTER_PAD_ANGLE)
+           );
+
+            // Curve outer radius
+            path.arc(0, 0, OUTER_RADIUS, angle + OUTER_PAD_ANGLE, delta - OUTER_PAD_ANGLE);
+
+            // Midway angle
+            path.lineTo(
+                MIDDLE_RADIUS * cos(delta + ARROW_ANGLE - MIDDLE_PAD_ANGLE),
+                MIDDLE_RADIUS * sin(delta + ARROW_ANGLE - MIDDLE_PAD_ANGLE)
+           );
+
+            // Curve inner radius
+            path.arc(0, 0, INNER_RADIUS, delta - INNER_PAD_ANGLE, angle + INNER_PAD_ANGLE, true);
+
+            // Midway angle
+            path.lineTo(
+                MIDDLE_RADIUS * cos(angle + ARROW_ANGLE + MIDDLE_PAD_ANGLE),
+                MIDDLE_RADIUS * sin(angle + ARROW_ANGLE + MIDDLE_PAD_ANGLE)
+            );
+
+            // Close the path
+            path.closePath();
+
+            // Return the path
+            return path;
+        }
+
+        //----- GET TEXT TRANSLATION -----------------------// 
+        function getTextTranslation(datum) {
             return 'translate(' + datum.centroid + ')';
-        });
+        }
 
-    //----- MANUAL WORD WRAPPING WITH TSPAN ------------// 
-    var tspan = text.selectAll('tspan')
-        .data(function (datum) {
-            var MAXIMUM_CHARS_PER_LINE = 10;
+        //----- GET TEXT LINES FOR WORD WRAPPING -----------// 
+        function getTextLines(datum) {
             var words = datum.label.split(' ');
             var lines = [words[0]];
             var linenum = 0;
 
             for (var i = 1, len = words.length; i < len; i++) {
                 var test = lines[linenum] + ' ' + words[i];
-                if (test.length <= MAXIMUM_CHARS_PER_LINE) {
+                if (test.length <= MAX_CHARS_PER_LINE) {
                     lines[linenum] = test;
                 } else {
                     lines[linenum] = lines[linenum].trim();
@@ -90,66 +177,17 @@
             }
 
             return lines;
-        })
-        .enter()
-        .append('tspan')
-            .attr('x', 0)
-            // With every new line, the entire thing needs to be moved up some
-            .attr('y', function (line, index, lines) {
-                return (2 * Math.SQRT2 * diameter * index - (lines.length - 1)) / 100
-            })
-            .text(function (line) {
-                return line.toUpperCase();
-            });
+        }
 
-     //----- CREATE A SECTION ---------------------------// 
-     function section(datum, index, paths) {
-         var angle = 2 * Math.PI / paths.length;
-         var nextIndex = index + 1;
+        //----- GET THE ADJUSTED TEXT TOP ------------------// 
+        function getAdjustedTextTop(line, index, lines) {
+            return (2 * SQRT2 * DIAMETER * index - lines.length - 1) / 100
+        }
 
-         // Calculate the text centroid
-         datum.centroid = centroid(
-             angle*index+arrowAngle+padAngle.midway, angle*nextIndex+arrowAngle-padAngle.midway,
-             radius.midway
-         );
+        //----- GET THE CURRENT LINE -----------------------// 
+        function getLine(line) {
+            return line;
+        }
+    };
 
-         var path = d3.path();
-
-         // Start
-         path.moveTo(
-             radius.outer * Math.cos(angle*index+padAngle.outer),
-             radius.outer * Math.sin(angle*index+padAngle.outer)
-        );
-
-         // Curve outer radius
-         path.arc(0, 0, radius.outer, angle*index+padAngle.outer, angle*nextIndex-padAngle.outer);
-
-         // Midway angle
-         path.lineTo(
-             radius.midway * Math.cos(angle*nextIndex+arrowAngle-padAngle.midway),
-             radius.midway * Math.sin(angle*nextIndex+arrowAngle-padAngle.midway)
-        );
-
-         // Curve inner radius
-         path.arc(0, 0, radius.inner, angle*nextIndex-padAngle.inner, angle*index+padAngle.inner, true);
-
-         // Midway angle
-         path.lineTo(
-             radius.midway * Math.cos(angle*index+arrowAngle+padAngle.midway),
-             radius.midway * Math.sin(angle*index+arrowAngle+padAngle.midway)
-         );
-
-         // Close the path
-         path.closePath();
-
-         return path;
-     }
-
-    //----- CALCULATE CENTROID -------------------------// 
-    function centroid(startAngle, endAngle, radius) {
-        var r = radius,
-            a = (startAngle + endAngle) / 2;
-        return [Math.cos(a) * r, Math.sin(a) * r];
-    }
-
-})(window.d3, window.Math, window.location);
+})(window.d3, window.Math.sin, window.Math.cos, 2*Math.PI, Math.SQRT2);
